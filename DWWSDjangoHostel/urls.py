1e13df7962a83c42d5260119332da415ee129a66
Aleph Campos da Silveira <alephcampos@gmail.com>
2021-02-10 15:47:52 -0300

    10/02/2021 DJANGO BLOCK MANIPULATION DATABASE LINK

--- DWWSDjangoHostel/urls.py
+++ DWWSDjangoHostel/urls.py
@@ -22,12 +22,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from pages.views import index_view, base_page_view, base_extension_view
+from register.views import register_detail_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('index/', index_view, name="Index_View"),
     path('', base_page_view, name="mo-Sidebar"),
-    path('base_extension/', base_extension_view, name="base_extension")
+    path('base_extension/', base_extension_view, name="base_extension"),
+    path('register/', register_detail_view, name="register")
 
 ]
 
--- register/views.py
+++ register/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Pessoa
+
 
 # Create your views here.
+
+def register_detail_view(request):
+    obj = Pessoa.objects.get(id=1)
+    context = {
+        'Nome': obj.nome,
+        'Perfil': obj.perfil,
+        'Idade': obj.idade
+
+    }
+
+
+    return render(request, "Pessoa/detail.html", context)
