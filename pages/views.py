73cbd118976de0caf7057a8b1608c27bc18633f7
Aleph Campos da Silveira <alephcampos@gmail.com>
2021-03-01 12:17:04 -0300

    10/02/2021 DJANGO BLOCK MANIPULATION DATABASE LINK 11/02/2021 CREATE USER 17/02/2021 RAW USER INPUT 18/02/2021 FIX 01/03/2021 Novo Template para LedsPlay

--- pages/views.py
+++ pages/views.py
@@ -7,22 +7,14 @@ from django.http import HttpResponse
 
 
 def index_view(request, *args, **kwargs):
-    return render(request, "index.html", {})
+    return render(request, "Industrial/index.html", {})
 
-
-def base_extension_view(request, *args, **kwargs):
-    my_context = {
-        "my_text": "isso é sobre mim",
-        "my_number": 123,
-        "my_list": ["123", "456", "789"],
-        "valores": [1, 2, 3, 4, 5, 6],
-        "html": "<h1> Ola </h1>"
-    }
-    return render(request, "base_extension.html", my_context)
+def generic_page_view(request, *args, **kwargs):
+    return render(request, "Industrial/generic.html", {})
 
 
 def base_page_view(request, *args, **kwargs):
-    return render(request, "base_page.html", {})
+    return render(request, "DWWS-Hostel/base_page.html", {})
 
 
 
