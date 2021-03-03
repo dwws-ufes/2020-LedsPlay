53686cca615e739dcf8f5cbcfe27ebedb5a5f558
Aleph Campos da Silveira <alephcampos@gmail.com>
2021-03-03 11:48:45 -0300

    03/03/2021
    VENV FIX

--- DWWS-LedsPlay/urls.py
+++ DWWS-LedsPlay/urls.py
@@ -1,4 +1,4 @@
-"""DWWSDjangoHostel URL Configuration
+"""DWWS-LedsPlay URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
--- DWWS-LedsPlay/wsgi.py
+++ DWWS-LedsPlay/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for DWWSDjangoHostel project.
+WSGI config for DWWS-LedsPlay project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DWWSDjangoHostel.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DWWS-LedsPlay.settings')
 
 application = get_wsgi_application()
