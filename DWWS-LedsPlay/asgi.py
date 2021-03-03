ab44d4784d65e0341b6394b8960b626e02f7aaf1
Aleph Campos da Silveira <alephcampos@gmail.com>
2021-03-03 11:48:45 -0300

    03/03/2021
    VENV FIX

--- DWWS-LedsPlay/asgi.py
+++ DWWS-LedsPlay/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for DWWSDjangoHostel project.
+ASGI config for DWWS-LedsPlay project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DWWSDjangoHostel.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DWWS-LedsPlay.settings')
 
 application = get_asgi_application()
