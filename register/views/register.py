80188e54cf385d4b8e7dca340018421937eefada
Davi Petris <davispetris@gmail.com>
2021-04-03 08:08:01 -0300

    modularizing register views

--- register/views/register.py
+++ register/views/register.py
@@ -1,51 +1,18 @@
-from django.http.response import HttpResponse, HttpResponseNotFound
+from django.http.response import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponseRedirect
 from django.urls.base import reverse_lazy
 from django.forms import inlineformset_factory
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
-from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth import logout
 
-from register.forms import PessoaForm, DefineUserForm, OrdemForm, CreateUserForm, LoginForm
+from register.forms import DefineUserForm, OrdemForm, CreateUserForm
 from django.urls import reverse
 from django.views import generic, View
-from .models import *
-from .filters import OrdemFilter
+from ..models import *
+from ..filters import OrdemFilter
 
-
-class LoginView(View):
-    def get(self, request):
-        data = {'form': LoginForm()}
-        return render(request, 'Pessoa/login.html', data)
-
-    def post(self, request):
-        form = LoginForm(data=request.POST)
-        
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            user = authenticate(username=username, password=password)
-            if username and password and user:
-                login(request, user)
-                if user.pessoa.user_type is None:
-                    return HttpResponseRedirect(reverse('register:define_user'))
-                return HttpResponseRedirect(reverse('index_view'))
-        
-        data = { 
-            'form': form,
-            'error': 'Usuário ou Senha incorreto'
-        }     
-        return render(request, 'Pessoa/login.html', data)
-
-class LogoutView(LoginRequiredMixin, View):
-    def get(self, request):
-        logout(request)
-        return HttpResponseRedirect(reverse('index_view'))
 
 class RegisterCreateView(SuccessMessageMixin, generic.CreateView):
    model = User
