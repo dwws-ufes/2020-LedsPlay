53454a33d637be9832d02507447d439941736b21
Davi Petris <davispetris@gmail.com>
2021-04-08 10:22:10 -0300

    move define user view to login.py

--- register/views/register.py
+++ register/views/register.py
@@ -65,44 +65,6 @@ class RegisterDeleteView(generic.DeleteView):
     template_name = "Pessoa/confirm_delete.html"
 
 
-class DefineUserTypeView(LoginRequiredMixin, View):
-    def get(self, request):
-        user = request.user
-        if user.pessoa.user_type is not None:
-            return HttpResponseRedirect(
-                reverse("index_view")
-            )  ## TODO: Retornar para a tela de configuração do profissional/cliente
-        data = {"form": DefineUserForm()}
-
-        return render(request, "Pessoa/define_user.html", data)
-
-    def post(self, request):
-        form = DefineUserForm(data=request.POST)
-        user = request.user
-
-        if form.is_valid():
-            if user.pessoa.user_type is not None:
-                return HttpResponseRedirect(
-                    reverse("index_view")
-                )  ## TODO: Retornar para a tela de configuração do profissional/cliente
-            choice = int(form.cleaned_data["selecione"])
-            if choice == 0:
-                from costumer.models import Cliente
-
-                subclass = Cliente
-            elif choice == 1:
-                from professional.models import Profissional
-
-                subclass = Profissional
-            else:
-                return HttpResponse(request, status=404)
-            user.pessoa.convert(subclass)
-            return HttpResponseRedirect(
-                reverse("index_view")
-            )  ## TODO: Retornar para a tela de configuração do profissional/cliente
-
-        return render(request, "Pessoa/define_user.html", {"form": form})
-
 @login_required(login_url="login")
 def register_competencia_view(request):
     competencias = Competencia.objects.all()
