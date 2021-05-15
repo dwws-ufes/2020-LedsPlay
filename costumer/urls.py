fa7dfff3744ac7f0611f54902fa0a6ed93416b68
AtilioA <27153776+AtilioA@users.noreply.github.com>
2021-05-14 23:40:32 -0300

    Create 'pseudocode' for RDF publication

--- costumer/urls.py
+++ costumer/urls.py
@@ -6,6 +6,7 @@ app_name = "costumer"
 urlpatterns = [
     path("dashboard/", views.CostumerDashboardView.as_view(), name="dashboard"),
     path("order/create/", views.CreateOrderView.as_view(), name="createOrdem"),
+    path("export/profissionais/", views.ExportProfessionalsView.as_view(), name="exportProfessionals"),
     path("order/update/<int:pk>", views.UpdateOrdemView.as_view(), name="updateOrdem"),
     path("order/delete/<int:pk>", views.DeleteOrdemView.as_view(), name="deleteOrdem"),
     path("edit/", views.UpdateClienteView.as_view(), name="edit"),
--- costumer/views.py
+++ costumer/views.py
@@ -10,6 +10,11 @@ from .filters import OrdemFilter
 from .forms import OrdemForm
 from django.shortcuts import render, redirect
 from django.contrib.auth.mixins import LoginRequiredMixin
+# from django.models import Profissional
+from professional.models import Profissional
+from rdflib.namespace import Graph, Namespace, URIRef, BNode, Literal, CSVW, DC, DCAT, DCTERMS, DOAP, FOAF, ODRL2, ORG, OWL, \
+                           PROF, PROV, RDF, RDFS, SDO, SH, SKOS, SOSA, SSN, TIME, \
+                           VOID, XMLNS, XSD
 
 
 class UpdateClienteView(LoginRequiredMixin, generic.UpdateView):
@@ -65,6 +70,44 @@ class CreateOrderView(LoginRequiredMixin, View):
             return redirect("costumer:dashboard")
         context = {"formset": formset}
         return render(request, "Dashboard/form.html", context)
+
+
+class ExportProfessionalsView(LoginRequiredMixin, View):
+    # OrderFormSet = inlineformset_factory(Cliente, Ordem, OrdemForm, extra=5)
+    # customer = None
+
+    def get(self, request):
+        g = Graph()
+        g.bind("foaf", FOAF)
+
+        n = Namespace("http://example.org/people/")
+
+        n.bob  # = rdflib.term.URIRef(u'http://example.org/people/bob')
+        n.eve  # = rdflib.term.URIRef(u'http://example.org/people/eve')
+
+        bob = URIRef("http://example.org/people/Bob")
+        linda = BNode()  # a GUID is generated
+
+        name = Literal('Bob')  # passing a string
+        age = Literal(24)  # passing a python int
+        height = Literal(76.5)  # passing a python float
+
+        g.add((bob, RDF.type, FOAF.Person))
+        g.add((bob, FOAF.name, name))
+        g.add((bob, FOAF.knows, linda))
+        g.add((linda, RDF.type, FOAF.Person))
+        g.add((linda, FOAF.name, Literal("Linda")))
+        profissionais = Profissional.objects.all()
+        print(profissionais)
+        print(FOAF.knows)
+        # self.customer = Cliente.objects.get(pk=request.user.pk)
+        # formset = self.OrderFormSet(
+        #     queryset=Ordem.objects.none(), instance=self.customer
+        # )
+        # context = {"formset": formset}
+        print(g.serialize(format="turtle").decode("utf-8"))
+        
+        return "Hello, World"
 
 
 class UpdateOrdemView(LoginRequiredMixin, generic.UpdateView):
